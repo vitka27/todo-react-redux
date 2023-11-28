a2e122cce9d880469eb2c7ff51a49e370cd87fb5
AMG <vitos162008@yandex.ru>
2023-11-28 22:27:58 +0300

    реализовал логику добавления задач в масив

--- src/components/AddInput/AddInput.jsx
+++ src/components/AddInput/AddInput.jsx
@@ -1,9 +1,13 @@
-function AddInput() {
+function AddInput({ setValue, onSubmit }) {
+  //! имеет смысл переделать на форму
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center">
-      <input type="text" className="form-control" />
-
-      <button className="btn" type="button">
+      <input
+        type="text"
+        onChange={(event) => setValue(event.target.value)}
+        className="form-control"
+      />
+      <button onClick={ (event) => onSubmit(event)} className="btn" type="button">
         <span className="bi bi-plus-square-dotted"></span>
       </button>
     </li>
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,23 +4,31 @@ import TodoItem from "../TodoItem/TodoItem";
 
 function TodoList() {
   const [todoList, settodoList] = useState([]);
+    const [value, setValue] = useState("");
+
+    const onSubmit = (event) =>{
+      event.preventDefault();
+      console.log('submit');
+      settodoList([...todoList, {title: value, completed: false}]);
+    }
 
   const getList = async () => {
+    console.log('fetch list');
     const response = await fetch("https://jsonplaceholder.typicode.com/todos");
     const list = await response.json();
     settodoList(list);
     console.log(list);
   };
 
-  useEffect(() => {
-    getList();
-  }, []);
+  // useEffect(() => {
+  //   getList();
+  // }, []);
 
   return (
-    <div className="wrapper container justify-content-center d-flex align-items-center flex-md-column w-25 ">
+    <div className="wrapper container justify-content-center d-flex align-items-center flex-md-column w-50 ">
       <h3 className=" pb-4">Список дел</h3>
       <ul className="list-group h-auto w-75">
-        <AddInput />
+        <AddInput setValue={setValue} onSubmit={onSubmit} />
         {todoList.map((item, id) => (
           <TodoItem key={id} item={item} />
         ))}
